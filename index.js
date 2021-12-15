bec513bda2876968cafa54d00155b71deaa0f2f1
Tenzin Jorden <tenden96@gmail.com>
2021-12-15 14:29:53 -0800

    commit tree a3ffa93d4a2531bc92e85806693eb7b300e4f4ce

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+
+function superbowlWin(record) {
+    const game = record.find(gameWon)
+    if(game) {
+        return game.year
+    }
+}
+
+function gameWon(game){
+    return game.result === "W";
+}
+
